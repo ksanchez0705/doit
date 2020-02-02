ea68fcfc4b5619f8486a5c9294c6994753715e10
kimberly sanchez <kxmbers22@gmail.com>
2020-02-02 18:41:25 +0000

    lesson 11

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe TasksController, type: :controller do
   describe "tasks#index" do
     it "should list the tasks in the database" do
-     task1 = FactoryBot.create(:task)
+      task1 = FactoryBot.create(:task)
       task2 = FactoryBot.create(:task)
       get :index
       expect(response).to have_http_status :success
@@ -11,4 +11,15 @@ RSpec.describe TasksController, type: :controller do
       expect(response_value.count).to eq(2)
     end
   end
+
+  describe "tasks#update" do
+    it "should allow tasks to be marked as done" do
+      task = FactoryBot.create(:task, done: false)
+      put :update, params: {id: task.id, task: { done: true }}
+      expect(response).to have_http_status(:success)
+      task.reload
+      expect(task.done).to eq(true)
+    end
+  end
+
 end
